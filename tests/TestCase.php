dffd994e33887620894f47d7680f0a4e2134543c
Nassif Bourguig <nbourguig@gmail.com>
2019-03-04 10:08:28 +0100

    feat: Add laravel 5.8 support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,14 +7,14 @@ class TestCase extends \Orchestra\Testbench\TestCase
     /** @var TestHelper */
     protected $helper;
 
-    public function setUp()
+    public function setUp() : void
     {
         parent::setUp();
 
         $this->helper = new TestHelper($this->app);
     }
 
-    public function tearDown()
+    public function tearDown() : void
     {
         parent::tearDown();
 
